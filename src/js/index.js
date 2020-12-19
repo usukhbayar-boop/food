3ce374bb23413c22fd110c44ffa354e52f1b8a84
usukhbayar otgonbat <capusukh16@gmail.com>
2020-12-19 13:12:16 +0800

    add state and search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,40 @@
 require('@babel/polyfill');
-import axios from 'axios';
 
+import Axios from 'axios';
 import Search from './model/search';
 
+/*
+* Web app state
+* - Хайлтын query, үр дүн
+* - Тухайн үзүүлж байгаа жор
+* - Лайкалсан жорууд
+* - Захиалж байгаа жорын найрлагууд
+*/
 
-let search = new Search('pizza');
+const state = {};
 
-search.doSearch().then( res => {
-    console.log(res);
-} )
+const controlSearch = async () => {
+    // 1. Вебээс хайлтын түлхүүр үгийг гаргаж гаргаж авна
+    const query = 'pizza';
+    
+    if(query) {
+    // 2. Шинээр хайлтын обьектийг үүсгэж өгнө.
+    state.search = new Search(query);
+
+
+    // 3. Хайлт хийхэд зориулж дэлгэцийг бэлтгэнэ
+
+    // 4. Хайлтыг гүйцэтгэнэ
+    await state.search.doSearch();
+
+    // 5. Хайлтын үр дүнг дэлгэцэнд үзүүлнэ
+    console.log(state.search.result);
+    } 
+
+    
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
